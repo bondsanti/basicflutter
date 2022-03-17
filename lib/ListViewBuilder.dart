8c82867374eebec5e32c03ad7fd2fd3fcad74a03
santi dev <santi.chpy@gmail.com>
2022-03-17 16:37:40 +0700

    menu

--- lib/ListViewBuilder.dart
+++ lib/ListViewBuilder.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'CafeMenu.dart';
 
 void main() {
   var app = MyApp();
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "โปรแกรมนับเลข",
+        title: "สั่งได้เลย!",
         home: MyHomePage(),
         theme: ThemeData(primarySwatch: Colors.green));
   }
@@ -23,18 +22,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<CafeMenu> menu = [
-    CafeMenu("เอสเปรสโซ่", "65",
-        "assets/image/a9237db1112a4bc09be59b49988c74ba.png"),
-    CafeMenu("แบล็คคอฟฟี่", "55",
-        "assets/image/e8fe5eb6cda2479493cd3e52fa84ba78.png"),
-    CafeMenu(
-        "คาปูชิโน่", "65", "assets/image/bef840806a5d4a1bad860b6940843976.png"),
-    CafeMenu(
-        "ชาเขียวนม", "60", "assets/image/0c1090e332a74b5db5ad5495c08bd229.png"),
-    CafeMenu(
-        "ชาไทย", "60", "assets/image/0a8707642c7740cd83d203b823b9344c.png"),
-  ];
+  int number = 0; //สร้าง State
 
   @override
   Widget build(BuildContext context) {
@@ -44,19 +32,14 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text("เลือกเมนู"), //เมนูด้านบนแอพ
         ),
         body: ListView.builder(
-            itemCount: menu.length,
+            itemCount: 15,
             itemBuilder: (BuildContext context, int index) {
-              CafeMenu items = menu[index];
               return ListTile(
-                leading: Image.asset(items.img),
                 title: Text(
-                  items.name,
+                  "เมนูที่ ${index + 1}",
                   style: TextStyle(fontSize: 20),
                 ),
-                subtitle: Text("ราคา " + items.price + " บาท"),
-                onTap: () {
-                  print("เลือก = " + items.name);
-                },
+                subtitle: Text("หัวข้อย้อย ${index + 1}"),
               );
             }));
   }
---
+++ lib/ListtileWidget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MyApp();
+  runApp(app);
+}
+
+//สร้าง Widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "โปรแกรมนับเลข",
+        home: MyHomePage(),
+        theme: ThemeData(primarySwatch: Colors.green));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0; //สร้าง State
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //home จะแสดงผลในหน้านี้
+      appBar: AppBar(
+        title: const Text("เลือกเมนู"), //เมนูด้านบนแอพ
+      ),
+      body: Center(
+        child: ListView(
+          children: getData(10),
+        ),
+      ),
+    );
+  }
+
+  //เตรียมข้อมูล
+  List<Widget> getData(int count) {
+    List<Widget> data = [];
+    for (var i = 0; i < count; i++) {
+      var menu = ListTile(
+        title: Text(
+          "เมนูที่ ${i + 1}",
+          style: TextStyle(fontSize: 20),
+        ),
+        subtitle: Text("หัวข้อย้อย ${i + 1}"),
+      );
+      data.add(menu);
+    }
+    return data;
+  }
+}
